ce4c62cfe9fbcdcd57a2efc65b359d1f102f3ed2
Emerance89 <tuyisengee472@gmail.com>
2024-07-06 13:07:55 -0700

    Eme

---
+++ lib/home.dart
@@ -0,0 +1,126 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'calculator_screen.dart';
+import 'drawer_navigation.dart';
+import 'theme_provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late StreamSubscription subscription;
+  bool isDeviceConnected = false;
+  bool isAlertSet = false;
+
+  @override
+  void initState() {
+    // Initialize connectivity
+    // getConnectivity();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    subscription.cancel();
+    super.dispose();
+  }
+
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    ScreenOne(),
+    CalculatorScreen(),
+    ScreenThree(),
+  ];
+
+  final List<String> _appBarTitles = ['Home', 'Calculator', 'About'];
+
+  void _onTabSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_appBarTitles[_selectedIndex]),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.read<ThemeStateProvider>().toggleTheme();
+            },
+            icon: context.watch<ThemeStateProvider>().isDarkTheme
+                ? Icon(Icons.dark_mode_outlined)
+                : Icon(Icons.light_mode_outlined),
+          ),
+        ],
+      ),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTabSelected,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: 'Calculator',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: 'About',
+          ),
+        ],
+      ),
+      drawer: DrawerNavigation(
+        user: null,
+        onItemSelected: _onTabSelected,
+        selectedIndex: _selectedIndex,
+      ),
+    );
+  }
+}
+
+class ScreenOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Text(
+            'Welcome to our platform! We are thrilled to have you join our community. Whether you are here to explore new ideas, connect with like-minded individuals, or embark on a journey of learning and growth, you have come to the right place. Our platform is designed to be a welcoming and inclusive space where everyone\'s voice is valued and respected. From informative discussions to engaging activities, welcome aboard!',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ScreenThree extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Text(
+            'At our core, we are a passionate team dedicated to fostering a vibrant and supportive community. With a commitment to excellence and innovation, we strive to create a platform where individuals from all walks of life can come together to learn, grow, and connect.',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
